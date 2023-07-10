0e6482b52dd98f1816c5b0e35bbbfead1d23a895
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-07-10 18:58:06 -0300

    feat: add POST task route

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,20 @@
+import { TasksEnum } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { authorId, description, type } = await request.json()
+  const task = await prisma.task.create({
+    data: {
+      authorId,
+      description,
+      type
+    }
+  });
+
+  if (!task) {
+    return new NextResponse("Task not created", { status: 500 });
+  }
+
+  return NextResponse.json({ task });
+}
