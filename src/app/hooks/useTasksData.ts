973a3afdbbe9444415aa8ad7898b39d79b1c56b5
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-07-11 10:59:24 -0300

    fix: remove uri from useTasksData fetchs

--- src/app/hooks/useTasksData.ts
+++ src/app/hooks/useTasksData.ts
@@ -10,25 +10,25 @@ const useTasksData = ({ authorId, type }: TasksDataProps) => {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   const getTasks = useCallback(() => {
-    fetch(`${process.env.API_URL}/api/tasks/${authorId}/${type}`)
+    fetch(`api/tasks/${authorId}/${type}`)
     .then((res) => res.json())
     .then((data) => setTasks(data))
   }, [authorId, type])
 
   const createTask = (body: any) => {
-    fetch(`${process.env.API_URL}/api/task`, { method: 'POST', body: JSON.stringify(body) })
+    fetch(`api/task`, { method: 'POST', body: JSON.stringify(body) })
       .then((res) => res.json())
       .then(() => getTasks())
   }
 
   const deleteTask = (id: string) => {
-    fetch(`${process.env.API_URL}/api/task/${id}`, { method: 'DELETE' })
+    fetch(`api/task/${id}`, { method: 'DELETE' })
     .then((res) => res.json())
     .then(() => getTasks())
   }
 
   const updateTask = (body: any) => {
-    fetch(`${process.env.API_URL}/api/task`, { method: 'PUT', body: JSON.stringify(body) })
+    fetch(`api/task`, { method: 'PUT', body: JSON.stringify(body) })
     .then((res) => res.json())
     .then(() => getTasks())
   }
